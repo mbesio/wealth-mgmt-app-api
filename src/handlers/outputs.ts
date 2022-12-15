8a2ac98577e420fc1e3c77388a2794311f9a638d
Matteo Besio <matteo.besio@gmail.com>
2022-12-14 21:05:36 -0800

    completed refactoring handlers

--- src/handlers/outputs.ts
+++ src/handlers/outputs.ts
@@ -1,13 +1,104 @@
 import prisma from '../server/db'
+import find from 'lodash.find'
+import { combineAmountsByDate, combineDates } from '../utils/inputs'
 
 export const getTimeseries = async (req, res) => {
+
+  const inputAssets = await prisma.inputAssets.findMany({})
+  const inputLiabilities = await prisma.inputLiabilities.findMany({})
+
+  // Map to {date, amountEUR, amountUSD}
+  const inputAssetsEURUSD = inputAssets.map(input => {
+    const { date, amount, fxVsUSD, fxVsEUR} = input
+    return {
+      date,
+      amountUSD: amount * fxVsUSD,
+      amountEUR: amount * fxVsEUR
+    }
+  })
+
+  const inputLiabilitiesEURUSD = inputLiabilities.map(input => {
+    const { date, remainingPrincipal, fxVsUSD, fxVsEUR} = input
+    return {
+      date,
+      amountUSD: remainingPrincipal * fxVsUSD,
+      amountEUR: remainingPrincipal * fxVsEUR
+    }
+  })
+
+  // Sum by date
+  const inputAssetsEURUSDByDate = combineAmountsByDate(inputAssetsEURUSD)
+  const inputLiabilitiesEURUSDByDate = combineAmountsByDate(inputLiabilitiesEURUSD)
+
+  // combine asset and input dates
+  const dates = combineDates(inputAssetsEURUSDByDate, inputLiabilitiesEURUSDByDate)
+
+  // map assets and liabilites to combined dates
+  const timeseries = dates.map(item => {
+    const assets = find(inputAssetsEURUSDByDate, {date: item.date}) || 0
+    const liabilites = find(inputLiabilitiesEURUSDByDate, {date: item.date}) || 0
+    return {
+      date: item.date,
+      assets: assets,
+      liabilities: liabilites,
+      netWorth: assets - liabilites
+    }
+  })
+
   res.json({
-    data: 'hello from getTimeseries'
+    data: timeseries
   })
 }
 
 export const getSummaryMetrics = async (req, res) => {
+  const currentDateObj = await prisma.inputAssets.findFirst({
+    orderBy: [{
+      date: 'desc',
+    }],
+    take: 1
+  })
+  const currentDate = currentDateObj.date
+  const currentAssets = await prisma.inputAssets.findMany({
+    where: {
+      date: currentDate
+    },
+    include: {
+      belongsTo: true
+    }
+  })
+  const currentLiabilites = await prisma.inputLiabilities.findMany({
+    where: {
+      date: currentDate
+    },
+    include: {
+      belongsTo: true
+    }
+  })
+
+  const currentAssetsUSDEUR = currentAssets.map(item => {
+    return {
+     name: item.belongsTo.name,
+     category: item.belongsTo.category,
+     currency: item.belongsTo.currency,
+     amountUSD: item.amount * item.fxVsUSD,
+     amountEUR: item.amount * item.fxVsEUR
+    }
+  })
+
+  const currentLiabilitiesUSDEUR = currentLiabilites.map(item => {
+    return {
+     name: item.belongsTo.name,
+     currency: item.belongsTo.currency,
+     amountUSD: item.remainingPrincipal * item.fxVsUSD,
+     amountEUR: item.remainingPrincipal * item.fxVsEUR
+    }
+  })
+
   res.json({
-    data: 'hello from getSummaryMetrics'
+    data: {
+      date: currentDate,
+      assets: currentAssetsUSDEUR,
+      liabilities: currentLiabilites,
+    }
   })
 }
--- src/utils/inputs.ts
+++ src/utils/inputs.ts
@@ -1,24 +1,34 @@
-import filter from 'lodash.filter'
-import { FX } from '@prisma/client'
-import { getFxRateTable, getFxRateUSD, getFxRateEUR } from '../utils/fx'
+import findIndex from 'lodash.findindex'
+import union from 'lodash.union'
 
-export const combineAssetsWithCurrencies = (inputAssetsFromPrimsa, fxFromPrisma) => {
-
-  const output = inputAssetsFromPrimsa.map( async (inputAsset) => {
-    const fxRates : FX[] = filter(fxFromPrisma, {date: inputAsset.date})
-    const fxRateTable = getFxRateTable(fxRates)
-    const currency = inputAsset.belongsTo.currency
-
-    const fxRateUSD = getFxRateUSD(currency, fxRateTable, inputAsset.date)
-    const fxRateEUR = getFxRateEUR(currency, fxRateTable, inputAsset.date, fxRateUSD)
-
-    const amountEUR = await fxRateUSD * inputAsset.amount
-    const amountUSD = await fxRateEUR * inputAsset.amount
-    return {
-      date: inputAsset.date,
-      amountEUR: amountEUR,
-      amountUSD: amountUSD
+/**
+  * Combines an array by the dates
+  * @param arrayDatesEURUSD is {
+  *  date: string,
+  *  amountUSD: number,
+  *  amountEUR: number
+  * }
+  * @returns array with the same object structure as input but summing per same dates
+  */
+export const combineAmountsByDate = (arrayDatesEURUSD) => {
+  return arrayDatesEURUSD.reduce((acc, item) => {
+    const currentDateIndex = findIndex(acc, {date: item.date})
+    if (currentDateIndex < 0) {
+      const newElement = {
+        date: item.date,
+        amountUSD: item.amountUSD,
+        amountEUR: item.amountEUR
+      }
+      acc.push(newElement)
+    } else {
+      acc[currentDateIndex].amountUSD = item.amountUSD
+      acc[currentDateIndex].amountEUR = item.amountEUR
     }
-  })
-  return output
+    return acc
+  },[])
+}
+
+export const combineDates = (dateArray1, dateArray2) => {
+  const combinedDates = union(dateArray1, dateArray2)
+  return combinedDates.sort((a ,b) => (a < b) ? -1 : ((a > b) ? 1 : 0))
 }
